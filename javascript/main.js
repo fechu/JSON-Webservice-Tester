94f8767bdc221e5cb82e0c374203b8c2cdcfa2c2
Sandro Meier <sandro.meier@fidelisfactory.ch>
2011-11-10 18:21:11 +0100

    Einige kleine Änderungen. Benennung ect.

--- javascript/main.js
+++ javascript/main.js
@@ -19,16 +19,31 @@ $(document).ready(function() {
 			data: jsonContent,
 			url: url,
 			type: 'POST', 
+			crossDomain: true,
 			success: function(data) {
 				// Show the result
 				$("#loading").slideUp();
 				$(".result").html('<pre>' + JSON.stringify(data, null, 2) + '</pre');
 				$("#result").slideDown();
 			}, 
-			error: function(jqXHR, textStatus, errorThrown) {
-				// Show the error
-				alert("Failed: " + textStatus);
-				$.unblockUI();
+			error: function (request, type, errorThrown)
+			{
+			    var message = "There was an error with the AJAX request.\n";
+			    switch (type) {
+			        case 'timeout':
+			            message += "The request timed out.";
+			            break;
+			        case 'notmodified':
+			            message += "The request was not modified but was not retrieved from the cache.";
+			            break;
+			        case 'parseerror':
+			            message += "XML/Json format is bad.";
+			            break;
+			        default:
+			            message += "HTTP Error (" + request.status + " " + request.statusText + ").";
+			    }
+			    message += "\n";
+			    alert(message);
 			}
 		});
 		
