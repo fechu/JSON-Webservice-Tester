dd3ad6f04d3c0cde666cc47878e20552a6872c80
Sandro Meier <sandro.meier@fidelisfactory.ch>
2012-02-06 13:50:35 +0100

    Implemented basic functions with new design. No saving of requests implemented yet.

--- javascript/main.js
+++ javascript/main.js
@@ -1,57 +1,59 @@
 $(document).ready(function() {
+
+	$('.dropdown-toggle').dropdown();
 	
 	$("#loading").hide();
 	$("#result").hide();
 	
-	// The send funciton
-	$("input[name=submit]").click(function() {
-		
-		// Slide out the form
-		$("#form").slideUp();
-		$("#loading").slideDown();
-		
-		var url = $("input[name=url]").val();
-		var jsonContent = $("textarea").val();
-		
-		// Start the ajax request
-		$.ajax({
-			contentType: 'application/json; charset=utf-8',
-			data: jsonContent,
-			url: url,
-			type: 'POST', 
-			crossDomain: true,
-			success: function(data) {
-				// Show the result
-				$("#loading").slideUp();
-				$(".result").html('<pre>' + JSON.stringify(data, null, 2) + '</pre');
-				$("#result").slideDown();
-			}, 
-			error: function (request, type, errorThrown)
-			{
-			    var message = "There was an error with the AJAX request.\n";
-			    switch (type) {
-			        case 'timeout':
-			            message += "The request timed out.";
-			            break;
-			        case 'notmodified':
-			            message += "The request was not modified but was not retrieved from the cache.";
-			            break;
-			        case 'parseerror':
-			            message += "XML/Json format is bad.";
-			            break;
-			        default:
-			            message += "HTTP Error (" + request.status + " " + request.statusText + ").";
-			    }
-			    message += "\n";
-			    alert(message + errorThrown);
-			    
-			    $("#loading").slideUp();
-			    $("#form").slideDown();
-			}
-		});
-		
-	});
 });
+
+function sendRequest()
+{
+	// Slide out the form
+	$("#form").slideUp();
+	$("#loading").slideDown();
+	
+	var url = $("#location").val();
+	var jsonContent = $("#json").val();
+	
+	// Start the ajax request
+	$.ajax({
+		contentType: 'application/json; charset=utf-8',
+		data: jsonContent,
+		url: url,
+		type: 'POST', 
+		crossDomain: true,
+		success: function(data) {
+			// Show the result
+			$("#loading").slideUp();
+			$("#response").html('<pre>' + JSON.stringify(data, null, 2) + '</pre');
+			$("#result").slideDown();
+		}, 
+		error: function (request, type, errorThrown)
+		{
+		    var message = "There was an error with the AJAX request.\n";
+		    switch (type) {
+		        case 'timeout':
+		            message += "The request timed out.";
+		            break;
+		        case 'notmodified':
+		            message += "The request was not modified but was not retrieved from the cache.";
+		            break;
+		        case 'parseerror':
+		            message += "XML/Json format is bad.";
+		            break;
+		        default:
+		            message += "HTTP Error (" + request.status + " " + request.statusText + ").";
+		    }
+		    message += "\n";
+		    alert(message + errorThrown);
+		    
+		    $("#loading").slideUp();
+		    $("#form").slideDown();
+		}
+	});
+	
+}
 
 function backToForm()
 {
